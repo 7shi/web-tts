669b243a1ed2e3d3ad9c6f5389c36c3aee99fa40
7shi <7shi@live.jp>
2020-07-02 19:40:02 +0900

    add `.speak()` (incompatible changes)

--- speech.js
+++ speech.js
@@ -4,14 +4,11 @@ function initVoices(languages) {
     if (languages) langs = languages;
     document.addEventListener("DOMContentLoaded", () => {
         for (let s of Array.from(document.getElementsByClassName("speak"))) {
-            let t = s.textContent;
-            s.speakTarget = getTarget(s);
-            let lang = s.getAttribute("language");
-            if (lang) {
-                s.language = langs[lang];
-                if (!t) t = s.language.name;
+            if (!s.textContent) {
+                s.language = langs[s.getAttribute("language")];
+                s.textContent = s.language.name;
             }
-            s.textContent = t;
+            if (!s.speak) s.speak = speakElem;
             s.addEventListener("click", () => speak(s));
         }
     });
@@ -41,62 +38,19 @@ function initVoices(languages) {
     setVoices();
 }
 
-function getTarget(elem) {
-    if (elem.getAttribute("speak")) return [elem];
-    for (let e = elem.nextSibling; e; e = e.nextSibling) {
-        if (e.nodeName == elem.nodeName) return [e];
-    }
-    return Array.from(elem.parentNode.parentNode.getElementsByTagName("td")).filter(e => e != elem && e.classList.contains("speak"));
-}
-
 var stopSpeaking = () => false;
 
 async function speak(elem) {
     if (!window.speechSynthesis) return true;
     let f = elem.classList.contains("speaking");
     if (stopSpeaking() && f) return true;
-    elem.classList.add("speaking");
-    if (elem.playStop) elem.textContent = elem.playStop[1];
-    let cancel = elem.speakTarget.length == 1
-        ? await speakLine(elem)
-        : await speakMany(elem.speakTarget);
-    if (elem.playStop) elem.textContent = elem.playStop[0];
-    elem.classList.remove("speaking");
-    return cancel;
-}
-
-async function speakLine(elem) {
-    let opt = elem.language.voice.selectedOptions;
-    if (!opt || opt.length == 0) return false;
-    let target = elem.speakTarget[0];
-    if (target != elem) target.classList.add("speaking2");
-    let cancel = target.words
-        ? await speakWords(elem.language, target.getElementsByTagName("span"))
-        : await speak1(elem.language, target);
-    if (target != elem) target.classList.remove("speaking2");
-    return cancel;
-}
-
-async function speakWords(lang, targets) {
-    for (let t of Array.from(targets)) {
-        t.classList.add("speaking-word");
-        let cancel = await speak1(lang, t);
-        t.classList.remove("speaking-word");
-        if (cancel) return true;
-    }
-    return false;
-}
-
-async function speakMany(targets) {
-    for (let t of targets) {
-        let cancel = await speak(t);
-        if (cancel) return true;
-    }
-    return false;
+    return await elem.speak();
 }
 
 function speak1(lang, target) {
     return new Promise((resolve, _) => {
+        let opt = lang.voice.selectedOptions;
+        if (!opt || opt.length == 0) return resolve(false);
         let speakend = cancel => {
             speakend = () => false;
             if (cancel) speechSynthesis.cancel();
@@ -117,53 +71,61 @@ function speak1(lang, target) {
     });
 }
 
-function setPlay(src, insert) {
-    let tables = src.getElementsByTagName("table");
-    if (tables.length) {
-        for (let table of Array.from(tables)) setPlay(table, insert);
-        return;
-    }
-    let src1 = src.getElementsByTagName("tr"), lang;
-    for (let i = 0; i < src1.length; i++) {
-        let td = src1[i].getElementsByTagName("td")[0];
-        if (insert && i >= insert) td = src1[i].insertBefore(document.createElement("td"), td);
-        let speak = td.classList.contains("speak");
-        let l = td.getAttribute("language");
-        let blank = !td.textContent.trim();
-        if (speak) { lang = l; if (blank) { td.textContent = langs[l].name; blank = false; } }
-        if ((speak && td.colSpan > 1) || (!td.childElementCount && blank)) {
-            td.classList.add("speak");
-            td.playStop = ["▶", "■"];
-            if (!blank) td.playStop = td.playStop.map(t => t + " " + td.textContent.trim()); else td.style.width = "1em";
-            td.textContent = td.playStop[0];
-            if (lang && !td.getAttribute("language")) td.setAttribute("language", lang);
+async function speakElem() {
+    this.classList.add("speaking");
+    if (this.playStop) this.textContent = this.playStop[1];
+    let cancel = false;
+    if (this.speakTarget) {
+        for (let t of this.speakTarget) {
+            if (await t.speak()) {
+                cancel = true;
+                break;
+            }
         }
+    } else {
+        cancel = await speak1(this.language, this);
+    }
+    if (this.playStop) this.textContent = this.playStop[0];
+    this.classList.remove("speaking");
+    return cancel;
+}
+
+function ensureVisible(margin, ...elems) {
+    let rs = elems.map(elem => elem.getBoundingClientRect());
+    let tp = Math.min(...rs.map(r => r.top   )) - margin;
+    let bt = Math.max(...rs.map(r => r.bottom)) + margin;
+    if (tp < 0) {
+        let top = pageYOffset + tp;
+        scroll({top: top, behavior: "smooth"});
+    } else if (bt > innerHeight) {
+        let top = pageYOffset + tp - (innerHeight - (bt - tp));
+        scroll({top: top, behavior: "smooth"});
     }
 }
 
-function makeTable(src, dst, skip) {
-    let src1 = src.getElementsByClassName("sentences");
-    let dst1 = dst.getElementsByClassName("sentences");
-    let src2 = [], langs = [];
-    for (let i = 0; i < src1.length; i++) {
-        let td = src1[i].getElementsByTagName("td");
-        let speaks = Array.from(td).filter(td => td.classList.contains("speak"));
-        langs.push(speaks.shift().getAttribute("language"));
-        src2.push(speaks.map(td => td.nextSibling));
-    }
-    if (skip) for (let i = 0; i < skip; i++) { src2.shift(); langs.shift(); }
-    for (let i = 0; i < dst1.length; i++) {
-        let tbody = dst1[i].getElementsByTagName("tr")[0].parentNode;
-        for (let j = 0; j < src2.length; j++) {
-            let tr = document.createElement("tr");
-            let td = document.createElement("td");
-            td.classList.add("speak");
-            td.setAttribute("language", langs[j]);
-            tr.appendChild(td);
-            let tdsrc = src2[j][i];
-            tdsrc.textContent = tdsrc.textContent;
-            tr.appendChild(tdsrc.cloneNode(true));
-            tbody.appendChild(tr);
-        }
-    }
+async function speakSpan() {
+    ensureVisible(innerHeight / 10, ...this.spans);
+    for (let sp of this.spans)
+        sp.classList.add(this == sp ? "speaking2" : "speaking3");
+    let cancel = await speak1(this.language, this);
+    for (let sp of this.spans)
+        sp.classList.remove(this == sp ? "speaking2" : "speaking3");
+    return cancel;
+}
+
+let spanTarget = null;
+
+function spanEnter(ev) {
+    if (spanTarget) spanLeave(null);
+    let t = ev.target;
+    if (!t.spans || t.classList.contains("speaking2") || t.classList.contains("speaking3"))
+        return;
+    for (let span of t.spans) span.classList.add("hovering");
+    spanTarget = ev.target;
+}
+
+function spanLeave(ev) {
+    if (ev && ev.target != spanTarget) return;
+    for (let span of spanTarget.spans) span.classList.remove("hovering");
+    spanTarget = null;
 }
